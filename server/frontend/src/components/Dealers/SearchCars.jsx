e2ae4ac6add6a1b6e30441c6d00e77866ac83980
Sklup55 <ksundararajan@skillup.tech>
2024-01-17 11:51:08 -0500

    completed frontedn Part 3 - whatever I did best

--- server/frontend/src/components/Dealers/SearchCars.jsx
+++ server/frontend/src/components/Dealers/SearchCars.jsx
@@ -28,24 +28,25 @@ const SearchCars = () => {
     fetchCars();
   }, [id]);
 
-  return (
+return (
+<div>
+    <Header />
+    <h1>Cars at Dealer ID {id}</h1>
     <div>
-      <Header />
-      <h1>Cars at Dealer ID {id}</h1>
-      <div>
-        {cars.length === 0 ? (
-          <p>Loading cars...</p>
-        ) : (
-          <div>
-            {cars.map((car) => (
-              <div key={car._id}>
-                <h3>{car.make} {car.model}</h3>
-                <p>Year: {car.year}</p>
-                <p>Mileage: {car.mileage}</p>
-                {/* Add other car details as needed */}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </div
+    {cars.length === 0 ? (
+        <p>Loading cars...</p>
+    ) : (
+        <div>
+        {cars.map((car) => (
+            <div key={car._id}>
+            <h3>{car.make} {car.model}</h3>
+            <p>Year: {car.year}</p>
+            <p>Mileage: {car.mileage}</p>
+            {/* Add other car details as needed */}
+            </div>
+        ))}
+        </div>
+    )}
+    </div>
+</div>
+); // Corrected the closing comment style here
