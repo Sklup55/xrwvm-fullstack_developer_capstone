53f0dfe3eef6a443a68625ee0ba0fc73e41fd4f2
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2024-01-17 17:01:41 +0530

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,27 +1,29 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import { Routes, Route, useParams } from "react-router-dom";  // Import useParams
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
-import PostReview from "./components/Dealers/PostReview"
-import DealerSearchWrapper from "./components/Dealers/DealerSearchWrapper"; // Import the DealerSearchWrapper component
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
+import DealerSearchWrapper from "./components/Dealers/DealerSearchWrapper";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/dealer/:id" element={<Dealer/>} />
-      <Route path="/dealer/:id"  element={
-           <>
-           <DealerSearchWrapper dealerId={id} /> {/* Render the DealerSearchWrapper component */}
-           <Dealer />
-           </>
+      <Route path="/dealers" element={<Dealers />} />
+      <Route
+        path="/dealer/:id"
+        element={
+          <>
+            <DealerSearchWrapper dealerId={useParams().id} /> {/* Render the DealerSearchWrapper component */}
+            <Dealer />
+          </>
         }
       />
-      <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/postreview/:id" element={<PostReview />} />
     </Routes>
   );
 }
+
 export default App;
